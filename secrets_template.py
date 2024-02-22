a8903f6b16e87dd638bbe67178378d035dce3260
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-02-22 10:14:34 -0500

    Remove need for OpenWeather account

--- secrets_template.py
+++ secrets_template.py
@@ -3,7 +3,6 @@
 # This file is where you keep secret settings, passwords, and tokens!
 # If you put them in the code you risk committing that info or sharing it
 # Create a free account at https://io.adafruit.com/ for your AIO username and key
-# Create a free account at https://openweathermap.org/ for your openweather token and location
 
 secrets = {
     'ssid' : 'SSID',
@@ -11,6 +10,4 @@ secrets = {
     'aio_username' : 'username',
     'aio_key' : 'api_key',
     'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
-    'openweather_token' : 'api_key',
-    'openweather_location' : 'New York, US',
     }
