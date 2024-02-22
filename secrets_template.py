6768107995014ad181fcb295da9b0652b0c9c230
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2024-02-22 10:12:29 -0500

    Add secrets.py template

---
+++ secrets_template.py
@@ -0,0 +1,16 @@
+# Rename this file to secrets.py
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# Create a free account at https://io.adafruit.com/ for your AIO username and key
+# Create a free account at https://openweathermap.org/ for your openweather token and location
+
+secrets = {
+    'ssid' : 'SSID',
+    'password' : 'PASSWORD',
+    'aio_username' : 'username',
+    'aio_key' : 'api_key',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'openweather_token' : 'api_key',
+    'openweather_location' : 'New York, US',
+    }
